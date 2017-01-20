7f0720ec3fb572b65dea0a2360f6180b45dd48b1
David Frank <bitinn@gmail.com>
2017-01-21 00:13:46 +0800

    main game controller

---
+++ Demo/Assets/FivePlusOne/GameJamDemo/Scripts/HamburgerController.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FivePlusOne.GGJ2017 {
+
+	public class HamburgerController : MonoBehaviour {
+
+	}
+
+}
