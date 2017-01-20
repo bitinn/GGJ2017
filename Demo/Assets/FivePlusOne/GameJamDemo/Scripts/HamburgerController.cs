0d5c9fc75dc1ec71f7ca9b88eea4588889789bd5
David Frank <bitinn@gmail.com>
2017-01-21 02:24:37 +0800

    allow live edit list of ingredient

--- Demo/Assets/FivePlusOne/GameJamDemo/Scripts/HamburgerController.cs
+++ Demo/Assets/FivePlusOne/GameJamDemo/Scripts/HamburgerController.cs
@@ -4,9 +4,41 @@ using System.Collections;
 using System.Collections.Generic;
 
 namespace FivePlusOne.GameJamDemo {
+	/*
+		main controller of the game
+	*/
 
 	public class HamburgerController : MonoBehaviour {
 
+		[Tooltip("List of hamburger ingredient")]
+		[SerializeField]
+		List<IngredientObject> ingredients;
+
 	}
 
+	/*
+		make ingredient easily editable in unity
+	*/
+
+	[System.Serializable]
+	public struct IngredientObject {
+		[SerializeField]
+		HamburgerIngredient _name;
+
+		[SerializeField]
+		GameObject _ingredient;
+
+		public HamburgerIngredient Name {
+			get { return _name; }
+		}
+
+		public GameObject Ingredient {
+			get { return _ingredient; }
+		}
+
+		public IngredientObject (HamburgerIngredient name, GameObject ingredient) {
+			_name = name;
+			_ingredient = ingredient;
+		}
+	}
 }
