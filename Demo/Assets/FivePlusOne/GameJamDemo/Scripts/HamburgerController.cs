d81dfe5f76642b86b41ad4ba1983fa688d3edbcf
David Frank <bitinn@gmail.com>
2017-01-21 00:53:27 +0800

    basic hamburger generator

--- Demo/Assets/FivePlusOne/GameJamDemo/Scripts/HamburgerController.cs
+++ Demo/Assets/FivePlusOne/GameJamDemo/Scripts/HamburgerController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace FivePlusOne.GGJ2017 {
+namespace FivePlusOne.GameJamDemo {
 
 	public class HamburgerController : MonoBehaviour {
 
---
+++ Demo/Assets/FivePlusOne/GameJamDemo/Scripts/HamburgerGenerator.cs
@@ -0,0 +1,38 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FivePlusOne.GameJamDemo {
+	/*
+		generate a sensible target hamburger for players to achieve
+	*/
+
+	public static class HamburgerGenerator {
+		/*
+			generate a list of ingredients given number of layers
+		*/
+
+		public static List<HamburgerIngredient> MakeBurger (int layerCount = 3) {
+			var burger = new List<HamburgerIngredient>();
+			var ingredients = System.Enum.GetValues(typeof(HamburgerIngredient));
+			var randomNumber = new System.Random();
+
+			for (var i = 0; i < layerCount; i++) {
+				var ingredient = (HamburgerIngredient) ingredients.GetValue(randomNumber.Next(0, ingredients.Length));
+				burger.Add(ingredient);
+			}
+
+			return burger;
+		}
+	}
+
+	/*
+		known hamburger ingredients
+	*/
+
+	[System.Serializable]
+	public enum HamburgerIngredient {
+		Pineapple, Meat, Lettuce, Pickles, Bacon, Onions, Cheese, Tomatoes
+	};
+}
